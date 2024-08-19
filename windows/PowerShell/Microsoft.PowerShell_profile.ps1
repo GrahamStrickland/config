2b2c825f621feaeaff0ac508d0bbf8b346287d62
Graham Strickland <grahamhstrickland@gmail.com>
2024-08-19 09:49:04 +0200

    Add PowerShell config

---
+++ windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+# PSRreadLine - https://github.com/PowerShell/PSReadLine
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+
+    # Binding for moving through history by prefix
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+}
+
+# PSFzf - https://github.com/kelleyma49/PSFzf and https://github.com/junegunn/fzf
+Import-Module PSFzf
+
+# Override PSReadLine's history search
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' `
+                -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Override default tab completion
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+
+# Oh My Posh - https://ohmyposh.dev/docs/installation/prompt
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/robbyrussell.omp.json" | Invoke-Expression
+
+function OpenIPython {
+	py -m IPython
+}
+
+Set-Alias ipython OpenIPython
+
