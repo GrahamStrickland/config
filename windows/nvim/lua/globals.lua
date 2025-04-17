8e15e2b296bc14a4ef22da1f8031d7f6a91eb5c4
Graham Strickland <grahamhstrickland@gmail.com>
2025-04-17 16:55:35 +0200

    Add setup options to lualine.nvim in Windows Neovim config

--- windows/nvim/lua/globals.lua
+++ windows/nvim/lua/globals.lua
@@ -52,7 +52,10 @@ vim.cmd("colorscheme vscode")
 
 require("lualine").setup({
     options = {
+        icons_enabled = false,
         theme = "vscode",
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '|', right = '|' }
     },
 })
 
