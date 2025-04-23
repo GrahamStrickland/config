e1874bd6dc2ac11f5a41080dfbd4f91746deac51
Graham Strickland <grahamhstrickland@gmail.com>
2025-04-23 09:44:43 +0200

    Fix nvim-cmp and luasnip keybinding conflicts in MacOS Neovim config

--- macos/.config/nvim/lua/keybindings.lua
+++ macos/.config/nvim/lua/keybindings.lua
@@ -266,25 +266,51 @@ vim.keymap.set(
 )
 
 -- LuaSnip keybindings -- see https://github.com/L3MON4D3/LuaSnip#keymaps
-vim.cmd[[
-" Use Tab to expand and jump through snippets
-imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? "<Plug>luasnip-expand-or-jump" : "<Tab>" 
-smap <silent><expr> <Tab> luasnip#jumpable(1) ? "<Plug>luasnip-jump-next" : "<Tab>"
+local ls = require("luasnip")
+local cmp = require("cmp")
 
-" Use Shift-Tab to jump backwards through snippets
-imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? "<Plug>luasnip-jump-prev" : "<S-Tab>"
-smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? "<Plug>luasnip-jump-prev" : "<S-Tab>"
-
-" Cycle forward through choice nodes with Control-f
-imap <silent><expre> <C-f> luasnip#choice_active() ? "<Plug>luasnip-next-choice" : "<C-f>"
-smap <silent><expre> <C-f> luasnip#choice_active() ? "<Plug>luasnip-next-choice" : "<C-f>"
-]]
-
-vim.keymap.set(
-    "n", 
-    "<leader>l", 
-    "<cmd>lua require('luasnip.loaders.from_lua').load({paths = '~/.config/nvim/LuaSnip/'})<cr>"
-)
+cmp.setup({
+    mapping = {
+        ["<cr>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                if ls.expandable() then
+                    ls.expand()
+                else
+                    cmp.confirm({
+                        select = true,
+                    })
+                end
+            else
+                fallback()
+            end
+        end),
+        ["<tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif ls.locally_jumpable(1) then
+                ls.jump(1)
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+        ["<s-tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif ls.locally_jumpable(-1) then
+                ls.jump(-1)
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+        ["<down>"] = cmp.mapping(function(fallback) 
+            if ls.choice_active() then
+                ls.change_choice(1)
+            else
+                fallback()
+            end
+        end, {"i", "s"})
+    }
+})
 
 -- VimTeX keybindings
 vim.keymap.set(
