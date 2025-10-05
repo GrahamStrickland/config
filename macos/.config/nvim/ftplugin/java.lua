87ab272bb93c374e4a31627022eb8cc0b12d9d52
Graham Strickland <grahamhstrickland@gmail.com>
2025-10-05 14:27:39 +0200

    Update jdtls version in macOS Neovim config

--- macos/.config/nvim/ftplugin/java.lua
+++ macos/.config/nvim/ftplugin/java.lua
@@ -1,5 +1,5 @@
 local config = {
-    cmd = { "/opt/homebrew/Cellar/jdtls/1.45.0/bin/jdtls" },
+    cmd = { "/opt/homebrew/Cellar/jdtls/1.51.0/bin/jdtls" },
     root_dir = vim.fs.dirname(vim.fs.find({"gradlew", ".git", "mvnw"}, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
