0c41e84cd2e4d00905a755e01ce7737d7f789a2c
Graham Strickland <grahamhstrickland@gmail.com>
2026-01-24 11:53:00 +0200

    Restore VSCode theme to Windows Neovim config

--- windows/nvim/lua/plugins.lua
+++ windows/nvim/lua/plugins.lua
@@ -3,7 +3,7 @@ local Plug = vim.fn["plug#"]
 vim.call("plug#begin")
 
 Plug("nvim-lualine/lualine.nvim")
-Plug("vague-theme/vague.nvim")
+Plug("Mofiqul/vscode.nvim")
 Plug("scrooloose/nerdtree")
 Plug("tpope/vim-dispatch")
 Plug("tpope/vim-unimpaired")
