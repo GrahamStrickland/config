7ccf2b0cc8a2e84675572454af4a3eba14afbc31
Graham Strickland <grahamhstrickland@gmail.com>
2026-01-29 09:35:11 +0200

    Fix erroneous plugins.lua in Windows Neovim config

--- windows/nvim/lua/plugins.lua
+++ windows/nvim/lua/plugins.lua
@@ -34,6 +34,7 @@ Plug("nvim-neotest/nvim-nio")
 Plug("rcarriga/nvim-dap-ui")
 Plug("hat0uma/csvview.nvim")
 Plug("seblyng/roslyn.nvim")
+Plug("HiPhish/rainbow-delimiters.nvim")
 Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
 
 vim.call("plug#end")
