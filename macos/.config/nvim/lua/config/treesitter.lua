4f41d23ef1e668848bb79c154716ea92bed41a1e
Graham Strickland <grahamhstrickland@gmail.com>
2026-01-31 09:37:24 +0200

    Fix treesitter not starting on buffer load causing rainbow-delimiters not to run in macOS Neovim config

--- macos/.config/nvim/lua/config/treesitter.lua
+++ macos/.config/nvim/lua/config/treesitter.lua
@@ -4,3 +4,12 @@ require("nvim-treesitter").setup({
     enable = true,
   },
 })
+
+-- Parse on buffer load to enable rainbow delimiters, see https://github.com/HiPhish/rainbow-delimiters.nvim/issues/187
+local function on_buf_win_enter(_args)
+    if vim.bo.filetype ~= "" then
+        vim.treesitter.start()
+    end
+end
+
+vim.api.nvim_create_autocmd("BufWinEnter", {pattern = "*", callback = on_buf_win_enter})
