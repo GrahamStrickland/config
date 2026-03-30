0246819855120c970eef81c1a58050fc74216e64
Graham Strickland <grahamhstrickland@gmail.com>
2026-03-30 10:59:36 +0200

    Fix Loogle search for Lean4 in macOS Neovim config

--- macos/.config/nvim/lua/plugins/lean.lua
+++ macos/.config/nvim/lua/plugins/lean.lua
@@ -1,7 +1,6 @@
 -- LEAN4 SETUP
 local lean = require("lean")
 local telescope = require("telescope")
-local bufnr = vim.api.nvim_get_current_buf()
 
 lean.setup { mappings = true }
 
@@ -9,5 +8,5 @@ vim.keymap.set(
     "n",
     "<leader>ls",
     function() telescope.extensions.loogle.loogle() end,
-    { noremap = true, silent = true, buffer = bufnr, }
+    { noremap = true, silent = true }
 )
