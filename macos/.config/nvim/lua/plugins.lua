d83a46a598150b01ffee4da2df8c2ab2df005e18
Graham Strickland <grahamhstrickland@gmail.com>
2026-04-11 14:45:42 +0200

    Cut fzf from macOS Neovim config

--- macos/.config/nvim/lua/plugins.lua
+++ macos/.config/nvim/lua/plugins.lua
@@ -7,8 +7,6 @@ vim.api.nvim_create_autocmd("PackChanged", {
         if name == "nvim-treesitter" and kind == "update" then
             if not ev.data.active then vim.cmd.packadd("nvim-treesitter") end
             vim.cmd("TSUpdate")
-        elseif name == "fzf" and kind == "update" then
-            vim.cmd("fzf#install()")
         elseif name == "LuaSnip" and kind == "update" then
             vim.cmd(":make install_jsregexp()")
         end
@@ -16,7 +14,6 @@ vim.api.nvim_create_autocmd("PackChanged", {
 })
 
 vim.pack.add({
-    gh("junegunn/fzf"),
     gh("nvim-lualine/lualine.nvim"),
     gh("vague2k/vague.nvim"),
     gh("scrooloose/nerdtree"),
