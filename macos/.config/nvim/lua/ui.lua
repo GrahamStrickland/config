46d7a1183fe03931c08535d35649983a85046ac6
Graham Strickland <grahamhstrickland@gmail.com>
2026-08-06 15:02:01 +0200

    Revert to retrobox colorscheme in macOS Neovim config

--- macos/.config/nvim/lua/ui.lua
+++ macos/.config/nvim/lua/ui.lua
@@ -1,6 +1,6 @@
 require("vim._core.ui2").enable({})
 
 vim.o.background = "dark"
-vim.cmd("colorscheme catppuccin")
+vim.cmd("colorscheme retrobox")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.o.winborder = "rounded"
