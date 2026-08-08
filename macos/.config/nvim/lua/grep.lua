3efb8468d27dffdecf2dc1e57bef73fe53957439
Graham Strickland <grahamhstrickland@gmail.com>
2026-08-08 13:23:48 +0200

    Change grep prompt in macOS Neovim config

--- macos/.config/nvim/lua/grep.lua
+++ macos/.config/nvim/lua/grep.lua
@@ -3,7 +3,7 @@ vim.opt.grepprg = "rg --vimgrep --smart-case --hidden"
 vim.opt.grepformat = "%f:%l:%c:%m"
 
 vim.keymap.set("n", "<leader>g", function()
-	vim.ui.input({ prompt = "Grep: " }, function(pattern)
+	vim.ui.input({ prompt = "ripgrep: " }, function(pattern)
 		if pattern then
 			vim.cmd("silent grep! " .. vim.fn.fnameescape(pattern))
 			vim.cmd("copen")
