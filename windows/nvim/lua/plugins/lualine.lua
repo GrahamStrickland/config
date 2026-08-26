53e521d094854f2a8af21490df400a4ada553ed5
Graham Strickland <grahamhstrickland@gmail.com>
2026-08-26 21:20:15 +0200

    Add searchcount to lualine setup in Neovim config

--- windows/nvim/lua/plugins/lualine.lua
+++ windows/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,17 @@ require("lualine").setup({
     options = {
         icons_enabled = true,
         theme = "auto",
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '|', right = '|' }
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "|", right = "|" }
     },
+    sections = {
+        lualine_z = {
+            "location",
+            {
+                "searchcount",
+                maxcount = 999,
+                timeout = 500,
+            }
+        }
+    }
 })
