28c41868a53aa8e82984d899a4cd57f0aebd18c9
Graham Strickland <grahamhstrickland@gmail.com>
2026-08-30 11:39:11 +0200

    Move to catppuccin theme in Neovim config

--- linux/.config/nvim/lua/ui.lua
+++ linux/.config/nvim/lua/ui.lua
@@ -1,6 +1,6 @@
 require("vim._core.ui2").enable({})
 
 vim.o.background = "dark"
-vim.cmd("colorscheme retrobox")
+vim.cmd("colorscheme catppuccin")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.o.winborder = "rounded"
